46654556fee668f5191d436402e3ab15a07edc94
Edwin Ong <edwin@Edwins-MacBook-Air.local>
2024-11-22 16:20:38 -0800

    tried to make website look better

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,34 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>
+      <body className="flex flex-col min-h-screen bg-yellow-100">
         <TRPCReactProvider>{children}</TRPCReactProvider>
+
+        <footer className="border-4 border-double border-black bg-zinc-50/50 rounded-lg mx-4 my-4 mt-auto">
+            <div className="w-full max-w-screen-xl mx-auto p-4 md-8">
+                <div className="sm:flex sm:items-center sm:justify-between">
+                    <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap text-black">Made by Edwin O.</span>
+                    </a>
+                    <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-black sm:mb-0">
+                        <li>
+                            <a href="https://www.edwin.ong" className="hover:underline me-4 md:me-6">Website</a>
+                        </li>
+                        <li>
+                            <a href="https://www.linkedin.com/in/edwin-dd-ong/" className="hover:underline me-4 md:me-6">LinkedIn</a>
+                        </li>
+                        <li>
+                            <a href="https://x.com/TheBestEdwinO" className="hover:underline me-4 md:me-6">Twitter</a>
+                        </li>
+                        <li>
+                            <a href="mailto:edwinong@alumni.stanford.edu" className="hover:underline">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+
+
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,24 +44,39 @@ export default function Home() {
   return (
     <>
       <main className="flex text-xl flex-col items-center animate-fade">
-        <div className="mt-8 w-full">
-            <p className="flex items-center justify-center">Upload an tournament mp4 file to remove commentary</p>
+        <div className="flex flex-col items-center justify-center w-full">
+            <p className="border-b-4 border-double border-black w-full bg-zinc-50/50 mx-4 mb-8 pt-8 pb-8 px-4 text-2xl font-bold text-center">Upload an tournament mp4 file to remove commentary</p>
 
-              <div className="flex flex-col items-center justify-center w-3/4 mx-auto">
-                  <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-gray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+              <div
+                className="flex flex-col items-center justify-center w-3/4 mx-auto"
+                onDragOver={(e) => {
+                    e.preventDefault(); // Prevent default behavior (open file)
+                }}
+                onDrop={(e) => {
+                    e.preventDefault(); // Prevent default behavior (open file)
+                    const file = e.dataTransfer.files[0]; // Get the dropped file
+                    if (file && file.type === "video/mp4") {
+                        setSelectedFile(file);
+                        console.log('Selected file:', file.name);
+                    } else {
+                        alert("Please drop an MP4 file");
+                    }
+                }}
+              >
+                  <label htmlFor="dropzone-file" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer  hover:bg-gray-800 bg-gray-700 border-gray-600 hover:border-gray-500">
                       <div className="flex flex-col items-center justify-center pt-5 pb-6">
                           <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
                           </svg>
                           <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                          <p className="text-xs text-gray-500 dark:text-gray-400">Ony accepts mp4</p>
+                          <p className="text-xs text-gray-500 dark:text-gray-400">Ony accepts .mp4</p>
                       </div>
                       <input id="dropzone-file" type="file" accept="video/mp4"onChange={handleFileUpload}className="hidden" />
                   </label>
                     {selectedFile && (
                         <button
                             onClick={handleDownload}
-                            className="mb-2 mt-4 px-4 py-2 bg-primary hover:bg-primary/70 text-blue-700 rounded-lg cursor-pointer transition-colors duration-300"
+                            className="font-bold mb-2 mt-4 px-4 py-2 bg-primary hover:bg-primary/70 text-blue-700 rounded-lg cursor-pointer transition-colors duration-300"
                         >
                             Click here to start processing {selectedFile.name}
                         </button>
